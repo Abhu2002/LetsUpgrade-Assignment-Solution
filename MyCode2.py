83fc077315d25f65d5da5860a8fcc2bdc41cbbec
Abhay Ashok Kapadnis <78680810+Abhu2002@users.noreply.github.com>
2021-04-06 23:35:29 +0530

    Add files via upload

---
+++ MyCode2.py
@@ -0,0 +1,27 @@
+a=1
+i=1
+while i<101:
+    if a%3==0 or a%5==0:
+        a=a+1
+    else :
+        print(a)
+        a=a+1
+    i=i+1
+w=1
+while w<6:
+   z=1
+   while z<6:
+      print ("*",end="")
+      z=z+1
+   w=w+1
+   print()
+n=1
+while n<=5:
+    print("ABHAY ",end="")
+    a=1
+    while a<=5 :
+        print("ROCKS ",end="")
+        a=a+1
+    n=n+1
+    print()
+
